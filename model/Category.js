c1393f4d558ab9e79699b51c86ae6aa28883151c
Muhammed Adhil K P <kpadhil61@gmail.com>
2026-03-16 08:08:34 +0530

    page not found route issue solve

--- model/Category.js
+++ model/Category.js
@@ -1,21 +1,2 @@
-// import mongoose from 'mongoose';
 
-// export interface ICategory extends Document {
-//   _id: ObjectId;
-//   CatgoryName: String;
-//   Timestamps: Timestamp | null;
-//   IsListed: Boolean | null;
-//   Description: String;
-// }
-
-// const CategorySchema: Schema = new Schema({
-//   CatgoryName: { type: String, required: true, unique: true },
-//   Timestamps: { type: Timestamp },
-//   IsListed: { type: Boolean },
-//   Description: { type: String, required: true },
-// });
-
-// const Category = mongoose.model<ICategory>('Category', CategorySchema);
-
-// export default Category;
 
